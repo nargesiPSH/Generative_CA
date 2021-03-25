9ce40aea86a09edc90d9a9843ebfef17e46eb3f8
nargesiPSH <79421500+nargesiPSH@users.noreply.github.com>
2021-03-25 12:28:46 +0000

    Update continauth.sh

--- continauth.sh
+++ continauth.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/vol/vssp/signsrc/externalLibs/cudnn-8.0-linux-x64-v6.0/lib64:$LD_LIBRARY_PATH                                                                                                                                                                          
-exec /vol/research/MCMC/anaconda3/envs/continauthH/bin/python /vol/research/MCMC/PyCharmCodes/Internship/ContinAuth-master/codes/chapter-5-5-siamese-cnn-preprocessing1.py
+exec /vol/research/MCMC/anaconda3/envs/continauthH/bin/python /vol/research/MCMC/PyCharmCodes/Internship/ContinAuth-master/codes/CA-system.py
+#make sure to update anaconda and python location to your local system file location
 
